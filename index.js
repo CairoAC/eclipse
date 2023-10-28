1f86b4266c78fe33c522748afaca3da519651c81
CairoAC <cairoayrescosta14@gmail.com>
2023-10-28 18:48:38 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const Eclipse = {
+  createElement: (type, props, ...children) => {
+    return { type, props: { ...props, children } };
+  },
+
+  render: (element, container) => {
+    if (typeof element === "string" || typeof element === "number") {
+      container.appendChild(document.createTextNode(element));
+      return;
+    }
+
+    const dom = document.createElement(element.type);
+
+    Object.keys(element.props || {}).forEach((name) => {
+      if (name !== "children") {
+        dom[name] = element.props[name];
+      }
+    });
+
+    if (element.props.children) {
+      element.props.children.forEach((child) => {
+        Eclipse.render(child, dom);
+      });
+    }
+
+    container.appendChild(dom);
+  },
+};
